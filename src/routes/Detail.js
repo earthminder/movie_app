3aae8e5de2ea39caf56dffd7f8331bf98f766bc6
CHS <coldputer@e4net.net>
2022-06-07 16:48:43 +0900

    code continue

---
+++ src/routes/Detail.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+// class Detail extends React.Component {
+
+//     componentDidMount() {
+//         const { location, history } = this.props;
+
+//         if (location.state === undefined) {
+//             history.push('/');
+//         }
+//     }
+//     render() {
+//         return <span>Hello</span>;
+//     }
+// }
+
+function Detail(props) {
+    console.log(props);
+    return <span>Hello</span>
+}
+
+export default Detail;
